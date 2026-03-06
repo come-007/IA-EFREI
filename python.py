df8141188936526c3570fc82a10a00fc27660394
come-007 <come.piernaz@efrei.net>
2026-03-06 10:16:35 +0100

    changes

--- python.py
+++ python.py
@@ -8,7 +8,13 @@ def grayscale ():
     Green = img[:, :, 1]
     Red = img[:, :, 2]
     grayscale_img = ((Blue+Green+Red)/3)
+    grayscale_img=np.where (grayscale_img<127,0,255)
     grayscale_img = grayscale_img.astype(np.uint8)
-    cv2.imwrite('nom_du_fichier.png', grayscale_img)
+    L=input("Largeur souhaité ?")
+    Lo=input("Longueur souhaité ?")
+    new_size=(L,Lo)
+    resized_img = cv2.resize(grayscale_img, new_size, interpolation=cv2.INTER_AREA)
+    nom_fichier=input("Quel nom pour votre fichier ?")
+    cv2.imwrite(f'{nom_fichier}.png', resized_img)
 
 grayscale()
